6d542e60442807e29cea45f250a9b9bca04937b4
rodrimati1992 <rodrimatt1985@gmail.com>
2019-06-01 16:22:30 -0300

    Implemented RObject (adding sabi_trait module) and finished prototype of ffi-safe trait object.

--- abi_stable/src/sabi_trait.rs
+++ abi_stable/src/sabi_trait.rs
@@ -1,9 +1,10 @@
 pub mod reexports{
 
     pub use std::{
-        marker::{PhantomData as __Phantom},
         ops::{Deref as __DerefTrait,DerefMut as __DerefMutTrait},
     };
+
+    pub use crate::marker_type::ErasedObject as __ErasedObject;
 
 
     pub mod __sabi_re{
@@ -12,14 +13,90 @@ pub mod reexports{
             prefix_type::{PrefixTypeTrait,WithMetadata},
             traits::IntoInner,
             sabi_types::{StaticRef,MovePtr},
+            sabi_trait::{
+                robject::{
+                    RObject,
+                },
+                vtable::{GetVTable,RObjectVtable,GetRObjectVTable},
+                for_generated_code::{sabi_from_ref,sabi_from_mut},
+            },
             utils::{transmute_reference,transmute_mut_reference,take_manuallydrop},
         };
 
-        pub use core_extensions::utils::transmute_ignore_size;
+        pub use core_extensions::{
+            utils::transmute_ignore_size,
+            TypeIdentity,
+        };
 
         pub use std::{
             mem::ManuallyDrop,
             ptr,
         };
     }
+}
+
+pub mod for_generated_code;
+pub mod robject;
+pub mod vtable;
+
+use std::marker::PhantomData;
+
+use self::{
+    reexports::{
+        *,
+        __sabi_re::*,
+    },
+    vtable::BaseVtable,
+};
+
+use crate::{
+    abi_stability::Tag,
+    erased_types::{c_functions,InterfaceType},
+    marker_type::ErasedObject,
+    type_level::bools::{True,False,Boolean},
+    sabi_types::MaybeCmp,
+    std_types::Tuple2,
+};
+
+
+pub mod markers{
+    use super::*;
+
+    use crate::{
+        std_types::utypeid::{UTypeId,no_utypeid,some_utypeid},
+        return_value_equality::ReturnValueEquality,
+    };
+
+
+    /// Indicates that a type does not implement `Any`.
+    pub struct YesImplAny;
+
+    /// Indicates that a type implements `Any`.
+    pub struct NoImplAny;
+    
+
+
+
+
+    /// Gets a function optionally returning the UTypeId of `T`.
+    /// Whether the function returns `MaybeCmp::Just(typeid)` is determined by `Self`.
+    pub trait GetUTID<T>{
+        const UID:ReturnValueEquality<MaybeCmp<UTypeId>>;
+    }
+
+
+    impl<T> GetUTID<T> for YesImplAny
+    where T:'static
+    {
+        const UID:ReturnValueEquality<MaybeCmp<UTypeId>>=ReturnValueEquality{
+            function:some_utypeid::<T>
+        };
+    }
+
+    impl<T> GetUTID<T> for NoImplAny{
+        const UID:ReturnValueEquality<MaybeCmp<UTypeId>>=ReturnValueEquality{
+            function:no_utypeid
+        };
+    }
+
 }
--- abi_stable/src/trait_object_test.rs
+++ abi_stable/src/trait_object_test.rs
@@ -17,19 +17,48 @@ use std::fmt::Debug;
 ///////                Generated Code                               ///////
 ///////////////////////////////////////////////////////////////////////////
 
-mod RSomething{
+pub use self::RSomething_items::{
+    __TraitObject as RSomething_TO,
+    __Methods as RSomething_Methods,
+    __TraitMarker as RSomething_Marker,
+    __Trait as RSomething,
+};
+
+mod RSomething_items{
     use super::*;
 
     use abi_stable::sabi_trait::reexports::{*,__sabi_re};
 
+    /// A marker type that represents RSomething in generic contexts.
     #[repr(C)]
-    pub struct TraitObject<'lt,P,Element,T>{
-        vtable:__sabi_re::StaticRef<VTable<(),P,Element,T>>,
-        ptr: __sabi_re::ManuallyDrop<P>,
-        _marker:__Phantom<&'lt ()>,
+    #[derive(StableAbi)]
+    pub struct __TraitMarker;
+
+    pub type __TraitObject<'lt,P,T,Element>=
+        __sabi_re::RObject<'lt,P,__TraitMarker,VTable<(),P,T,Element>>;
+
+    mod __inside_{
+        use super::__TraitMarker;
+        use abi_stable::{InterfaceType,type_level::bools::*};
+
+        abi_stable::impl_InterfaceType!{
+            impl InterfaceType for __TraitMarker{
+                type Send=False;
+                type Sync=True;
+                type Clone=True;
+            }
+        }
     }
 
-    pub trait Trait<T>{
+    ///
+    /// # Generated trait object
+    ///
+    /// The generated trait object only implements this trait if it implements
+    /// the {insert which Deref trait here} trait.
+    ///
+    /// To only require Deref in methods taking `&self` you can use the 
+    /// `{trait_name}_Methods` trait.
+    pub trait __Trait<T>{
         type Element:Debug;
 
         fn get(&self)->&Self::Element;
@@ -38,78 +67,54 @@ mod RSomething{
         where Self:Sized;
     }
 
-    pub fn new<'lt,P,_Self,T>(ptr:P)-> TraitObject<'lt,P::TransmutedPtr,_Self::Element,T>
-    where 
-        P:__DerefTrait<Target=_Self>+'lt,
-        P:__sabi_re::TransmuteElement<()>,
-        _Self:Trait<T>+'lt,
-    {
-        let vtable=MakeVTable::<_Self,P,P::TransmutedPtr,T>::VTABLE;
-        let ptr=unsafe{
-            __sabi_re::TransmuteElement::<()>::transmute_element(ptr,__Phantom)
-        };
-        let ptr=__sabi_re::ManuallyDrop::new(ptr);
-        unsafe{
-            TraitObject{
-                vtable:__sabi_re::transmute_ignore_size(vtable),
-                ptr,
-                _marker:__Phantom,
-            }
-        }
+    /// A duplicate of the trait definition,
+    /// to be able to call methods with the most permissive pointer traits.
+    pub trait __Methods<__ErasedPtr,T>{
+        type Element_:Debug;
+
+        fn get_(&self)->&Self::Element_
+        where
+            __ErasedPtr: __DerefTrait<Target=()>;
+        
+        fn get_mut_(&mut self)->&mut Self::Element_
+        where
+            __ErasedPtr: __DerefMutTrait<Target=()>;
+
+        fn into_value_(self)->Self::Element_
+        where
+            __ErasedPtr: __sabi_re::OwnedPointer<Target=()>;
     }
 
-    impl<'lt,P,Element,T> TraitObject<'lt,P,Element,T>
+    impl<'lt,__ErasedPtr,T,Element>  
+        __Methods<__ErasedPtr,T> 
+    for __TraitObject<'lt,__ErasedPtr,T,Element>
     where
         Element:Debug,
     {
-        fn _as_ref(&self)->&__Phantom<()>
-        where
-            P: __DerefTrait<Target=()>
-        {
-            unsafe{&*((&**self.ptr) as *const () as *const __Phantom<()>)}
-        }
-        fn _as_mut(&mut self)->&mut __Phantom<()>
-        where
-            P: __DerefMutTrait<Target=()>
-        {
-            unsafe{&mut *((&mut **self.ptr) as *mut () as *mut __Phantom<()>)}
-        }
+        type Element_=Element;
 
-        #[inline]
-        fn _sabi_with_value<F,R>(self,f:F)->R
-        where 
-            P: __sabi_re::OwnedPointer<Target=()>,
-            F:FnOnce(__sabi_re::MovePtr<'_,()>)->R,
-        {
-            let mut __this= __sabi_re::ManuallyDrop::new(self);
-            __sabi_re::OwnedPointer::with_moved_ptr(
-                unsafe{ __sabi_re::ptr::read(&mut __this.ptr) },
-                f,
-            )
-        }
-
-        pub fn get_(&self)->&Element
+        fn get_(&self)->&Self::Element_
         where
-            P: __DerefTrait<Target=()>
+            __ErasedPtr: __DerefTrait<Target=()>
         {
-            self.vtable.get().get()(self._as_ref())
+            self.sabi_vtable().get()(self.sabi_erased_ref())
         }
-        pub fn get_mut_(&mut self)->&mut Element
+        fn get_mut_(&mut self)->&mut Self::Element_
         where
-            P: __DerefMutTrait<Target=()>
+            __ErasedPtr: __DerefMutTrait<Target=()>
         {
-            self.vtable.get().get_mut()(self._as_mut())
+            self.sabi_vtable().get_mut()(self.sabi_erased_mut())
         }
-        pub fn into_value_(self)->Element
+        fn into_value_(self)->Self::Element_
         where
-            P: __sabi_re::OwnedPointer<Target=()>
+            __ErasedPtr: __sabi_re::OwnedPointer<Target=()>
         {
-            let __method=self.vtable.get().into_value();
-            self._sabi_with_value(move|__this|__method(__this))
+            let __method=self.sabi_vtable().into_value();
+            self.sabi_with_value(move|_self|__method(_self))
         }
     }
 
-    impl<'lt,P,Element,T> Trait<T> for TraitObject<'lt,P,Element,T>
+    impl<'lt,P,T,Element> __Trait<T> for __TraitObject<'lt,P,T,Element>
     where
         P: __DerefMutTrait<Target=()>,
         P: __sabi_re::OwnedPointer<Target=()>,
@@ -129,81 +134,161 @@ mod RSomething{
     }
 
 
-    impl<P,Element,T> Drop for TraitObject<'_,P,Element,T>{
-        fn drop(&mut self){
-            let __method=self.vtable.get()._sabi_drop();
-            unsafe{
-                __method(abi_stable::utils::take_manuallydrop(&mut self.ptr))
-            }
-        }
-    }
-
-
     #[repr(C)]
     #[derive(StableAbi)]
     #[sabi(kind(Prefix(prefix_struct="VTable")))]
-    struct VTableVal<_Self,_ErasedPtr,Element,T>{
-        _sabi_tys:__Phantom<extern "C" fn(_Self,_ErasedPtr,Element,T)>,
+    pub struct VTableVal<_Self,_ErasedPtr,T,Element>{
+        _sabi_tys: ::std::marker::PhantomData<extern "C" fn(_Self,_ErasedPtr,T,Element)>,
 
-        _sabi_drop:extern "C" fn(this:_ErasedPtr),
+        _sabi_vtable:__sabi_re::StaticRef<__sabi_re::RObjectVtable<_ErasedPtr,__TraitMarker>>,
 
-        get:extern "C" fn(this:&__Phantom<_Self>)->&Element,
+        get:extern "C" fn(_self:&__ErasedObject<_Self>)->&Element,
         
-        get_mut:extern "C" fn(this:&mut __Phantom<_Self>)->&mut Element,
+        get_mut:extern "C" fn(_self:&mut __ErasedObject<_Self>)->&mut Element,
 
         #[sabi(last_prefix_field)]
-        into_value:extern "C" fn(this:__sabi_re::MovePtr<'_,_Self>)->Element,
+        into_value:extern "C" fn(_self:__sabi_re::MovePtr<'_,_Self>)->Element,
+    }
+
+    unsafe impl<IA,_Self,_ErasedPtr,_OrigPtr,T> 
+        __sabi_re::GetVTable<IA,_Self,_ErasedPtr,_OrigPtr,T>
+    for __TraitMarker
+    where
+        _Self:__Trait<T>,
+        __TraitMarker:__sabi_re::GetRObjectVTable<IA,_Self,_ErasedPtr,_OrigPtr>,
+    {
+        type VTable=VTable<(),_ErasedPtr,T,_Self::Element>;
+
+        fn get_vtable()->__sabi_re::StaticRef<Self::VTable>{
+            unsafe{
+                let vtable:*const VTable<_Self,_ErasedPtr,T,_Self::Element>=
+                    (*MakeVTable::<IA,_Self,_ErasedPtr,_OrigPtr,T>::VTABLE).as_prefix_raw();
+                __sabi_re::StaticRef::from_raw(vtable as *const Self::VTable)
+            }
+        }
     }
 
 
-    struct MakeVTable<_Self,_ErasedPtr,_OrigPtr,T>(_Self,_ErasedPtr,_OrigPtr,T);
+    struct MakeVTable<IA,_Self,_ErasedPtr,_OrigPtr,T>(IA,_Self,_ErasedPtr,_OrigPtr,T);
 
 
-    impl<_Self,_ErasedPtr,_OrigPtr,T> MakeVTable<_Self,_ErasedPtr,_OrigPtr,T>
+    impl<IA,_Self,_ErasedPtr,_OrigPtr,T> MakeVTable<IA,_Self,_ErasedPtr,_OrigPtr,T>
     where 
-        _Self:Trait<T>,
+        _Self:__Trait<T>,
+        __TraitMarker:__sabi_re::GetRObjectVTable<IA,_Self,_ErasedPtr,_OrigPtr>,
     {
-        const VTABLE: *const __sabi_re::WithMetadata<VTableVal<_Self,_ErasedPtr,_Self::Element,T>>= 
-            &__sabi_re::WithMetadata::new(
-                __sabi_re::PrefixTypeTrait::METADATA,
-                VTableVal{
-                    _sabi_tys:__Phantom,
-                    _sabi_drop:Self::_sabi_drop,
-                    get:Self::get,
-                    get_mut:Self::get_mut,
-                    into_value:Self::into_value,
-                }
-            );
+        const VTABLE: *const __sabi_re::WithMetadata<
+            VTableVal<_Self,_ErasedPtr,T,_Self::Element>
+        >={
+            let __vtable=VTableVal{
+                _sabi_tys: ::std::marker::PhantomData,
+                _sabi_vtable:__sabi_re::GetRObjectVTable::ROBJECT_VTABLE,
+                get:Self::get,
+                get_mut:Self::get_mut,
+                into_value:Self::into_value,
+            };
+            &__sabi_re::WithMetadata::new(__sabi_re::PrefixTypeTrait::METADATA,__vtable)
+        };
 
-        #[inline]
-        fn __unerase_ptr(this:_ErasedPtr)->_OrigPtr{
-            unsafe{ __sabi_re::transmute_ignore_size(this) }
-        }
-        extern "C" fn _sabi_drop(this:_ErasedPtr){
-            ::abi_stable::extern_fn_panic_handling!{
-                Self::__unerase_ptr(this);
-            }
-        }
-    
-        extern "C" fn get(this:&__Phantom<_Self>)->&_Self::Element{
-            ::abi_stable::extern_fn_panic_handling!{
-                let _self=unsafe{ __sabi_re::transmute_reference::<_,_Self>(this) };
-                Trait::get(_self)
+        extern "C" fn get(_self:&__ErasedObject<_Self>)->&_Self::Element{
+            unsafe{
+                __sabi_re::sabi_from_ref(
+                    _self,
+                    move|_self| __Trait::get(_self)
+                )
             }
         }
 
-        extern "C" fn get_mut(this:&mut __Phantom<_Self>)->&mut _Self::Element{
-            ::abi_stable::extern_fn_panic_handling!{
-                let _self=unsafe{ __sabi_re::transmute_mut_reference::<_,_Self>(this) };
-                Trait::get_mut(_self)
+        extern "C" fn get_mut(_self:&mut __ErasedObject<_Self>)->&mut _Self::Element{
+            unsafe{
+                __sabi_re::sabi_from_mut(
+                    _self,
+                    move|_self| __Trait::get_mut(_self)
+                )
             }
         }
 
-        extern "C" fn into_value(this:__sabi_re::MovePtr<'_,_Self>)->_Self::Element {
-            ::abi_stable::extern_fn_panic_handling!{
-                Trait::into_value(this.into_inner())
+        extern "C" fn into_value(_self:__sabi_re::MovePtr<'_,_Self>)->_Self::Element {
+            ::abi_stable::extern_fn_panic_handling!{no_early_return;
+                __Trait::into_value(_self.into_inner())
             }
         }
     }    
 }
 
+
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    use crate::std_types::RBox;
+
+    impl RSomething<()> for u32{
+        type Element=u32;
+
+        fn get(&self)->&Self::Element{
+            self
+        }
+        fn get_mut(&mut self)->&mut Self::Element{
+            self
+        }
+        fn into_value(self)->Self::Element
+        where Self:Sized
+        {
+            self
+        }
+    }
+
+    fn assert_sync<T:Sync>(_:&T){}
+
+    #[test]
+    fn construct_trait_object(){
+        let mut object=RSomething_TO::from_value_unerasable::<_,()>(100_u32);
+        let mut erased=RSomething_TO::from_value::<_,()>(100_u32);
+        
+        assert_sync(&object);
+        assert_sync(&erased);
+
+        fn assertions_unerased(mut object:RSomething_TO<'_,RBox<()>,(),u32>){
+            assert_eq!(object.sabi_as_unerased::<u32>().ok(),Some(&100));
+            assert_eq!(object.sabi_as_unerased::<i8>().ok(),None::<&i8>);
+            assert_eq!(object.sabi_as_unerased_mut::<u32>().ok(),Some(&mut 100));
+            assert_eq!(object.sabi_as_unerased_mut::<i8>().ok(),None::<&mut i8>);
+            object=object.sabi_into_unerased::<i8>().unwrap_err().into_inner();
+            assert_eq!(object.sabi_into_unerased::<u32>().ok(),Some(RBox::new(100)));
+        }
+
+        fn assertions_erased(mut object:RSomething_TO<'_,RBox<()>,(),u32>){
+            assert_eq!(object.sabi_as_unerased::<u32>().ok(),None);
+            assert_eq!(object.sabi_as_unerased::<i8>().ok(),None);
+            assert_eq!(object.sabi_as_unerased_mut::<u32>().ok(),None);
+            assert_eq!(object.sabi_as_unerased_mut::<i8>().ok(),None);
+            object=object.sabi_into_unerased::<u32>().unwrap_err().into_inner();
+            object=object.sabi_into_unerased::<i8>().unwrap_err().into_inner();
+        }
+
+        assertions_unerased(object.clone());
+        assertions_unerased(object);
+        
+        assertions_erased(erased.clone());
+        assertions_erased(erased);
+        
+        
+    }
+
+    #[test]
+    fn trait_object_methods(){
+        let mut object=RSomething_TO::from_value::<_,()>(100);
+        let mut cloned=object.clone();
+        
+        assert_eq!(object.get_(),&100);
+        assert_eq!(object.get_mut_(),&mut 100);
+        assert_eq!(object.into_value_(),100);
+
+        assert_eq!(cloned.get(),&100);
+        assert_eq!(cloned.get_mut(),&mut 100);
+        assert_eq!(cloned.into_value(),100);
+    }
+}
