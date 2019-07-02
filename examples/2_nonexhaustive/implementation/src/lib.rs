ffc73e06a54cad0a7a5fbd7de3979e4cccd16984
rodrimati1992 <rodrimatt1985@gmail.com>
2019-07-02 01:16:26 -0300

    Added nonexhaustive enum example crates,fixed example READMES.

---
+++ examples/2_nonexhaustive/implementation/src/lib.rs
@@ -0,0 +1,191 @@
+use std::{
+    collections::HashMap,
+    mem,
+};
+
+use abi_stable::{
+    nonexhaustive_enum::{NonExhaustiveFor,NonExhaustive},
+    prefix_type::PrefixTypeTrait,
+    sabi_trait::prelude::TU_Opaque,
+    std_types::{RBox,RBoxError,RResult,RStr,RString,ROk,RErr,RVec},
+    extern_fn_panic_handling,
+    export_root_module,
+    rtry,
+};
+
+use core_extensions::SelfOps;
+
+use example_2_interface::{
+    Cents,Command,Error,ItemId,ReturnVal,Shop,Shop_TO,Shop_from_value,ShopMod,ShopModVal,
+    SerdeWrapper,
+};
+
+
+#[export_root_module]
+extern fn instantiate_root_module()->&'static ShopMod{
+    extern_fn_panic_handling!{
+        ShopModVal {
+            new,
+            deserialize_command,
+            deserialize_ret_val,
+        }.leak_into_prefix()
+    }
+}
+
+pub extern "C" fn new()->Shop_TO<'static,RBox<()>>{
+    extern_fn_panic_handling!{
+        Shop_from_value::<_,TU_Opaque>(
+            ShopState{
+                items_map:HashMap::default(),
+                items:Vec::new(),
+            }
+        )
+    }
+}
+
+
+
+#[derive(Debug,Clone)]
+struct ShopState{
+    items_map:HashMap<RString,usize>,
+    items:Vec<Item>
+}
+
+#[derive(Debug,Clone)]
+struct Item{
+    name:RString,
+    id:ItemId,
+    price:Cents,
+    count:u32,
+}
+
+
+
+impl Shop for ShopState{
+    fn run_command(
+        &mut self,
+        cmd:NonExhaustiveFor<Command>,
+    )->RResult<NonExhaustiveFor<ReturnVal>,NonExhaustiveFor<Error>>{
+        extern_fn_panic_handling!{
+
+            use std::collections::hash_map::Entry;
+
+            match cmd.into_enum() {
+                Ok(Command::CreateItem{name,initial_count:count,price})=>{
+                    match self.items_map.entry(name.clone()) {
+                        Entry::Occupied(entry)=>{
+                            let id=ItemId{id:*entry.get()};
+                            return RErr(NonExhaustive::new(Error::ItemAlreadyExists{id,name}))
+                        }
+                        Entry::Vacant(entry)=>{
+                            let id=ItemId{id:self.items.len()};
+                            entry.insert(self.items.len());
+                            self.items.push(Item{name,id,price,count});
+                            ReturnVal::CreateItem{count,id}
+                        }
+                    }
+                }
+                Ok(Command::DeleteItem{id})=>{
+                    if id.id < self.items.len() {
+                        self.items.remove(id.id);
+                        ReturnVal::DeleteItem{id}
+                    }else{
+                        return RErr(NonExhaustive::new(Error::ItemIdNotFound{id}));
+                    }
+                }
+                Ok(Command::AddItem{id,count})=>{
+                    match self.items.get_mut(id.id) {
+                        Some(item) => {
+                            item.count+=count;
+                            ReturnVal::AddItem{remaining:item.count,id}
+                        }
+                        None => {
+                            return RErr(NonExhaustive::new(Error::ItemIdNotFound{id}));
+                        }
+                    }
+                }
+                Ok(Command::RemoveItem{id,count})=>{
+                    match self.items.get_mut(id.id) {
+                        Some(item) => {
+                            let prev_count=item.count;
+                            item.count=item.count.saturating_sub(count);
+                            ReturnVal::RemoveItem{
+                                removed:prev_count-item.count,
+                                remaining:item.count,
+                                id
+                            }
+                        }
+                        None => {
+                            return RErr(NonExhaustive::new(Error::ItemIdNotFound{id}));
+                        }
+                    }
+                }
+                Ok(Command::RenameItem{id,new_name})=>{
+                    match self.items.get_mut(id.id) {
+                        Some(item) => {
+                            let old_name=mem::replace(&mut item.name,new_name.clone());
+                            self.items_map.remove(&old_name);
+                            self.items_map.insert(new_name.clone(),id.id);
+                            ReturnVal::RenameItem{id,old_name,new_name}
+                        }
+                        None => {
+                            return RErr(NonExhaustive::new(Error::ItemIdNotFound{id}));
+                        }
+                    }
+                }
+                Ok(Command::Many{list})=>{
+                    let mut ret=RVec::with_capacity(list.len());
+                    for elem in list {
+                        let ret_cmd=rtry!(self.run_command(elem.inner));
+                        ret.push(SerdeWrapper::new(ret_cmd));
+                    }
+                    ReturnVal::Many{list:ret}
+                }
+                Ok(Command::__NonExhaustive)=>{
+                    return 
+                        Error::InvalidCommand{
+                            cmd:RBox::new(NonExhaustive::new(Command::__NonExhaustive))
+                        }.piped(NonExhaustive::new)
+                        .piped(RErr);
+                }
+                Err(e)=>{
+                    return 
+                        Error::InvalidCommand{
+                            cmd:RBox::new(e.into_inner())
+                        }.piped(NonExhaustive::new)
+                        .piped(RErr);
+                }
+            }.piped(NonExhaustive::new)
+                .piped(ROk)
+        }
+    }
+}
+
+
+
+extern "C" fn deserialize_command(s:RStr<'_>)->RResult<NonExhaustiveFor<Command>,RBoxError>{
+    extern_fn_panic_handling!{
+        deserialize_json::<Command>(s.into())
+            .map(NonExhaustiveFor::new)
+    }
+}
+
+extern "C" fn deserialize_ret_val(s:RStr<'_>)->RResult<NonExhaustiveFor<ReturnVal>,RBoxError>{
+    extern_fn_panic_handling!{
+        deserialize_json::<ReturnVal>(s.into())
+            .map(NonExhaustiveFor::new)
+    }
+}
+
+
+
+
+fn deserialize_json<'a, T>(s: RStr<'a>) -> RResult<T, RBoxError>
+where
+    T: serde::Deserialize<'a>,
+{
+    match serde_json::from_str::<T>(s.into()) {
+        Ok(x) => ROk(x),
+        Err(e) => RErr(RBoxError::new(e)),
+    }
+}
---
+++ examples/2_nonexhaustive/interface/src/lib.rs
@@ -0,0 +1,227 @@
+use abi_stable::{
+    nonexhaustive_enum::{NonExhaustiveFor,DeserializeOwned,GetEnumInfo,InterfaceBound},
+    library::RootModule,
+    sabi_types::{VersionStrings},
+    std_types::{RBox,RString,RResult,RStr,RBoxError,RVec},
+    type_level::bools::True,
+    sabi_trait,
+    StableAbi,InterfaceType,
+    package_version_strings,
+    declare_root_module_statics,
+    impl_InterfaceType,
+};
+
+#[cfg(feature="v1_1")]
+use serde::{Deserialize,Deserializer};
+
+use serde_json::value::RawValue;
+
+
+#[repr(transparent)]
+#[cfg_attr(feature="v1_1",derive(Deserialize))]
+#[derive(StableAbi,Debug,Clone,Copy,PartialEq)]
+pub struct Cents{
+    pub cents:u64,
+}
+
+#[repr(transparent)]
+#[cfg_attr(feature="v1_1",derive(Deserialize))]
+#[derive(StableAbi,Debug,Clone,Copy,PartialEq)]
+pub struct ItemId{
+    #[doc(hidden)]
+    pub id:usize,
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+#[repr(transparent)]
+#[cfg(feature="v1_1")]
+#[derive(StableAbi,Debug,Clone,PartialEq)]
+pub struct SerdeWrapper<T>{
+    pub inner:T,
+}
+
+#[cfg(feature="v1_1")]
+impl<T> SerdeWrapper<T>{
+    pub fn new(inner:T)->Self{
+        Self{inner}
+    }
+}
+
+
+#[cfg(feature="v1_1")]
+impl<'de,T> Deserialize<'de> for SerdeWrapper<NonExhaustiveFor<T>>
+where
+    T: GetEnumInfo+'de,
+    T::DefaultInterface: DeserializeOwned<T,T::DefaultStorage,T::DefaultInterface>,
+    T::DefaultInterface: InterfaceBound<Deserialize=True>,
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = <&RawValue>::deserialize(deserializer)?;
+        NonExhaustiveFor::<T>::deserialize_owned_from_str(s.get())
+            .map(|x| SerdeWrapper{inner:x} )
+            .map_err(serde::de::Error::custom)
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+#[repr(u8)]
+#[cfg_attr(feature="v1_1",derive(Deserialize))]
+#[derive(StableAbi,Debug,Clone,PartialEq)]
+#[sabi(kind(WithNonExhaustive(
+    size="[usize;8]",
+    traits(Debug,Clone,PartialEq,Deserialize),
+)))]
+pub enum Command{
+    #[doc(hidden)]
+    __NonExhaustive,
+    CreateItem{
+        name:RString,
+        initial_count:u32,
+        price:Cents,
+    },
+    DeleteItem{
+        id:ItemId,
+    },
+    AddItem{
+        id:ItemId,
+        count:u32,
+    },
+    RemoveItem{
+        id:ItemId,
+        count:u32,
+    },
+    #[cfg(feature="v1_1")]
+    RenameItem{
+        id:ItemId,
+        new_name:RString,
+    },
+    #[cfg(feature="v1_1")]
+    Many{
+        list:RVec<SerdeWrapper<NonExhaustiveFor<Command>>>
+    },
+}
+
+
+impl DeserializeOwned<Command,Command_Storage,Command_Interface> for Command_Interface{
+    fn deserialize_enum(s: RStr<'_>) -> Result<NonExhaustiveFor<Command>, RBoxError>{
+        ShopMod::get_module().unwrap().deserialize_command()(s).into_result()
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+#[repr(u8)]
+#[cfg_attr(feature="v1_1",derive(Deserialize))]
+#[derive(StableAbi,Debug,Clone,PartialEq)]
+#[sabi(kind(WithNonExhaustive(
+    size=64,
+    interface="Command_Interface",
+)))]
+pub enum ReturnVal{
+    #[doc(hidden)]
+    __NonExhaustive,
+    CreateItem{
+        count:u32,
+        id:ItemId,
+    },
+    DeleteItem{
+        id:ItemId,
+    },
+    AddItem{
+        remaining:u32,
+        id:ItemId,
+    },
+    RemoveItem{
+        removed:u32,
+        remaining:u32,
+        id:ItemId,
+    },
+    #[cfg(feature="v1_1")]
+    RenameItem{
+        id:ItemId,
+        new_name:RString,
+        old_name:RString,
+    },
+    #[cfg(feature="v1_1")]
+    Many{
+        list:RVec<SerdeWrapper<NonExhaustiveFor<ReturnVal>>>
+    },
+}
+
+
+impl DeserializeOwned<ReturnVal,ReturnVal_Storage,Command_Interface> for Command_Interface{
+    fn deserialize_enum(s: RStr<'_>) -> Result<NonExhaustiveFor<ReturnVal>, RBoxError>{
+        ShopMod::get_module().unwrap().deserialize_ret_val()(s).into_result()
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+#[repr(u8)]
+#[derive(StableAbi,Debug,Clone,PartialEq)]
+#[sabi(kind(WithNonExhaustive(
+    size="[usize;6]",
+    traits(Debug,Clone,PartialEq),
+)))]
+pub enum Error{
+    #[doc(hidden)]
+    __NonExhaustive,
+    ItemAlreadyExists{
+        id:ItemId,
+        name:RString,
+    },
+    ItemIdNotFound{
+        id:ItemId,
+    },
+    InvalidCommand{
+        cmd:RBox<NonExhaustiveFor<Command>>,
+    },
+}
+
+
+#[repr(C)]
+#[derive(StableAbi)] 
+#[sabi(kind(Prefix(prefix_struct="ShopMod")))]
+#[sabi(missing_field(panic))]
+pub struct ShopModVal {
+    pub new:extern "C" fn()->Shop_TO<'static,RBox<()>>,
+
+    pub deserialize_command:
+        extern "C" fn(s:RStr<'_>)->RResult<NonExhaustiveFor<Command>,RBoxError>,
+
+    #[sabi(last_prefix_field)]
+    pub deserialize_ret_val:
+        extern "C" fn(s:RStr<'_>)->RResult<NonExhaustiveFor<ReturnVal>,RBoxError>,
+}
+
+
+impl RootModule for ShopMod {
+    declare_root_module_statics!{ShopMod}
+    const BASE_NAME: &'static str = "shop";
+    const NAME: &'static str = "shop";
+    const VERSION_STRINGS: VersionStrings = package_version_strings!();
+}
+
+
+
+#[sabi_trait]
+pub trait Shop{
+    #[sabi(last_prefix_field)]
+    fn run_command(
+        &mut self,
+        cmd:NonExhaustiveFor<Command>,
+    ) -> RResult<NonExhaustiveFor<ReturnVal>,NonExhaustiveFor<Error>>;
+}
