abec1e35a7fcfc4a4aae43e75c64c96f7039073e
rodrimati1992 <rodrimatt1985@gmail.com>
2019-08-20 11:31:25 -0300

    Added docs and example for the ExtraCheck combination API.

--- abi_stable/src/abi_stability/extra_checks.rs
+++ abi_stable/src/abi_stability/extra_checks.rs
@@ -386,6 +386,291 @@ impl std::error::Error for UnequalConstError{}
 
 ```
 
+
+# Combination
+
+This is how an ExtraChecks can be combined across all 
+dynamic libraries to ensure some property(which can be relied on for safety).
+
+This is a very similar process to how abi_stable ensures that 
+vtables and modules are consistent across dynamic libraries.
+
+### Failure
+
+Loading many libraries that contain ExtraChecks trait objects that need 
+to be combined can fail if the representative version of the trait objects 
+are incompatible with those of the library,
+even if both the library and the binary are otherwise compatible.
+
+The graphs bellow uses the `LIBRARY( ExtraChecks trait object )` format,
+where the trait object is compatible only if the one in the binary 
+is a prefix of the string in the library,
+and all the libraries have a prefix of the same string.
+
+This is fine:
+
+```text
+A("ab")<---B("abc")
+\__________C("abcd")
+```
+
+This is not fine
+
+```text
+ __________D("abe")
+/
+A("ab")<---B("abc")
+\__________C("abcd")
+```
+
+
+### Example
+
+```
+
+use abi_stable::{
+    abi_stability::{
+        check_layout_compatibility,
+        TypeChecker_TO,
+        ExtraChecks,ExtraChecksStaticRef,ExtraChecks_TO,CombineResult,
+        ExtraChecksExt,ExtraChecksError
+    },
+    marker_type::UnsafeIgnoredType,
+    type_layout::TypeLayout,
+    sabi_trait::prelude::TU_Opaque,
+    std_types::{RCow,RResult,RSome,StaticStr},
+    sabi_extern_fn,
+    GetStaticEquivalent,
+    StableAbi,
+};
+
+use std::fmt::{self,Display};
+
+
+const LAYOUT0:&'static TypeLayout= <WithConstant<V1_0> as StableAbi>::LAYOUT;
+const LAYOUT1:&'static TypeLayout= <WithConstant<V1_1> as StableAbi>::LAYOUT;
+const LAYOUT1B:&'static TypeLayout=<WithConstant<V1_1_Incompatible> as StableAbi>::LAYOUT;
+const LAYOUT2:&'static TypeLayout= <WithConstant<V1_2> as StableAbi>::LAYOUT;
+
+
+fn main(){
+    // Compared LAYOUT0 to LAYOUT1B,stored LAYOUT0.extra_checks associated with both layouts.
+    check_layout_compatibility(LAYOUT0,LAYOUT1B).unwrap();
+
+    // Compared LAYOUT1 to LAYOUT2,stored LAYOUT2.extra_checks associated with both layouts.
+    check_layout_compatibility(LAYOUT1,LAYOUT2).unwrap();
+
+    // Compared LAYOUT0 to LAYOUT2:
+    // - the comparison succeeded,
+    // - then both are combined.
+    // - The combined trait object is attempted to be combined with the
+    //      ExtraChecks in the global map associated to both LAYOUT0 and LAYOUT2,
+    //      which are LAYOUT1B.extra_checks and LAYOUT2.extra_checks respectively.
+    // - Combining the trait objects with the ones in the global map fails because 
+    //      the one from LAYOUT1B is incompatible with the one from LAYOUT2.
+    check_layout_compatibility(LAYOUT0,LAYOUT2).unwrap_err();
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+#[repr(C)]
+#[derive(StableAbi)]
+#[sabi(
+    // Replaces the C:StableAbi constraint with `C:GetStaticEquivalent` 
+    // (a supertrait of StableAbi).
+    not_stableabi(C),
+    bound="C:GetConstant",
+    extra_checks="Self::get_const_checker"
+)]
+struct WithConstant<C>{
+    // UnsafeIgnoredType is equivalent to PhantomData,
+    // except that all `UnsafeIgnoredType` are considered the same type by `StableAbi`.
+    _marker:UnsafeIgnoredType<C>,
+}
+
+impl<C> WithConstant<C>{
+    const NEW:Self=Self{
+        _marker:UnsafeIgnoredType::NEW,
+    };
+}
+
+impl<C> WithConstant<C>
+where 
+    C:GetConstant
+{
+    const CHECKER:&'static ConstChecker=
+        &ConstChecker{
+            chars:StaticStr::new(C::CHARS)
+        };
+
+    #[sabi_extern_fn]
+    pub fn get_const_checker()->ExtraChecksStaticRef{
+        ExtraChecksStaticRef::from_ptr(
+            Self::CHECKER,
+            TU_Opaque,
+        )
+    }
+}
+
+
+trait GetConstant{
+    const CHARS:&'static str;
+}
+
+use self::constants::*;
+
+#[allow(non_camel_case_types)]
+mod constants{
+    use super::*;
+    
+    #[derive(GetStaticEquivalent)]
+    pub struct V1_0;
+
+    impl GetConstant for V1_0{
+        const CHARS:&'static str="ab";
+    }
+
+
+    #[derive(GetStaticEquivalent)]
+    pub struct V1_1;
+
+    impl GetConstant for V1_1{
+        const CHARS:&'static str="abc";
+    }
+
+
+    #[derive(GetStaticEquivalent)]
+    pub struct V1_1_Incompatible;
+
+    impl GetConstant for V1_1_Incompatible{
+        const CHARS:&'static str="abd";
+    }
+
+
+    #[derive(GetStaticEquivalent)]
+    pub struct V1_2;
+
+    impl GetConstant for V1_2{
+        const CHARS:&'static str="abcd";
+    }
+}
+
+
+
+/////////////////////////////////////////
+
+#[repr(C)]
+#[derive(Debug,Clone,StableAbi)]
+pub struct ConstChecker{
+    chars:StaticStr,
+}
+
+
+impl Display for ConstChecker{
+    fn fmt(&self,f:&mut fmt::Formatter<'_>)->fmt::Result{
+        writeln!(
+            f,
+            "ConstChecker: \
+                Checks that the associated constant for \
+                the other type is compatible with:\n{}\n.\
+            ",
+            self.chars
+        )
+    }
+}
+
+
+impl ConstChecker {
+    fn check_compatible_inner(&self,other:&ConstChecker)->Result<(), UnequalConstError> {
+        if other.chars.starts_with(&*self.chars) {
+            Ok(())
+        }else{
+            Err(UnequalConstError{
+                expected:self.chars,
+                found:other.chars,
+            })
+        }
+    }
+}
+impl ExtraChecks for ConstChecker {
+    fn type_layout(&self)->&'static TypeLayout{
+        <Self as StableAbi>::LAYOUT
+    }
+
+    fn check_compatibility(
+        &self,
+        _layout_containing_self:&'static TypeLayout,
+        layout_containing_other:&'static TypeLayout,
+        checker:TypeChecker_TO<'_,&mut ()>,
+    )->RResult<(), ExtraChecksError> {
+        Self::downcast_with_layout(layout_containing_other,checker,|other|{
+            self.check_compatible_inner(other)
+        })
+    }
+
+    fn nested_type_layouts(&self)->RCow<'_,[&'static TypeLayout]>{
+        RCow::from_slice(&[])
+    }
+
+    fn combine(
+        &self,
+        other:ExtraChecks_TO<'static,&()>,
+        checker:TypeChecker_TO<'_,&mut ()>
+    )->CombineResult{
+        Self::downcast_with_object(other,checker,|other|{
+            let (min,max)=min_max_by(self,other,|x|x.chars.len());
+            min.check_compatible_inner(max)
+                .map(|_| RSome( ExtraChecks_TO::from_value(max.clone(),TU_Opaque) ) )
+        })
+    }
+}
+
+
+
+#[derive(Debug,Clone)]
+pub struct UnequalConstError{
+    expected:StaticStr,
+    found:StaticStr,
+}
+
+impl Display for UnequalConstError{
+    fn fmt(&self,f:&mut fmt::Formatter<'_>)->fmt::Result{
+        writeln!(
+            f,
+            "Expected the `GetConstant::CHARS` associated constant to be compatible with:\
+             \n    {}\
+             \nFound:\
+             \n    {}\
+            ",
+            self.expected,
+            self.found,
+        )
+    }
+}
+
+impl std::error::Error for UnequalConstError{}
+
+
+pub(crate) fn min_max_by<T,F,K>(l:T,r:T,mut f:F)->(T,T)
+where 
+    F:FnMut(&T)->K,
+    K:Ord,
+{
+    if f(&l) < f(&r) {
+        (l,r)
+    }else{
+        (r,l)
+    }
+}
+
+
+
+```
+
 */
 #[sabi_trait]
 //#[sabi(debug_print_trait)]
