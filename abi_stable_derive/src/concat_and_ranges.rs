cf998ecfd8f1548b7b14033f6a921383f9768447
rodrimati1992 <rodrimatt1985@gmail.com>
2019-09-11 15:54:40 -0300

    Added `concatenated_and_ranges` macro,and tokenization functions for RSlice and RStr literals.

---
+++ abi_stable_derive/src/concat_and_ranges.rs
@@ -0,0 +1,87 @@
+use syn::{
+    parse::{Parse, ParseStream},
+    punctuated::Punctuated,
+    Error, Ident, Token,
+};
+
+use proc_macro2::TokenStream as TokenStream2;
+
+use quote::quote;
+
+///////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, Clone)]
+struct StringAndVariable {
+    variable: Ident,
+    string: String,
+}
+
+#[derive(Debug, Clone)]
+struct ConcatenatedStrings {
+    concatenated: Ident,
+    strings: Punctuated<StringAndVariable, Token![,]>,
+}
+
+impl Parse for StringAndVariable {
+    fn parse(input: ParseStream) -> Result<Self, Error> {
+        let variable = input.parse()?;
+        let _: Token![=] = input.parse()?;
+        let string = input.parse::<syn::LitStr>()?.value();
+
+        Ok(Self { variable, string })
+    }
+}
+
+impl Parse for ConcatenatedStrings {
+    fn parse(input: ParseStream) -> Result<Self, Error> {
+        let concatenated = input.parse::<Ident>()?;
+
+        let paren_tokens;
+        let _ = syn::parenthesized!(paren_tokens in input);
+
+        let strings = paren_tokens.parse_terminated(Parse::parse)?;
+
+        Ok(Self {
+            concatenated,
+            strings,
+        })
+    }
+}
+
+pub fn macro_impl(input: TokenStream2) -> Result<TokenStream2,syn::Error> {
+    let ConcatenatedStrings {
+        concatenated: conc_ident,
+        strings,
+    }=syn::parse2::<ConcatenatedStrings>(input)?;
+    
+    let capacity = strings.iter().map(|sav| sav.string.len() + 1).sum();
+
+    let mut concatenated = String::with_capacity(capacity);
+    let mut starts = Vec::<u16>::with_capacity(strings.len());
+    let mut lengths = Vec::<u16>::with_capacity(strings.len());
+
+    for sav in &strings {
+        let start=concatenated.len() as u16;
+        starts.push(start);
+        concatenated.push_str(&sav.string);
+        lengths.push(concatenated.len() as u16-start);
+        concatenated.push(';');
+    }
+
+    let concat_len = concatenated.len();
+    let variable = strings.iter().map(|x| &x.variable);
+
+    Ok(quote!(
+        use abi_stable::{
+            std_types::RStr,
+            type_layout::StartLen,
+        };
+        pub const #conc_ident:RStr<'static>=unsafe{
+            RStr::from_raw_parts( #concatenated.as_ptr() ,#concat_len )
+        };
+
+        #(
+            pub const #variable:StartLen= StartLen{ start:#starts, len:#lengths };
+        )*
+    ))
+}
---
+++ abi_stable_derive/src/literals_constructors.rs
@@ -0,0 +1,61 @@
+//! This module contains helper types/functions to output 
+//! the literals and constructors for types that don't implement ToTokens.
+
+use crate::{
+    to_token_fn::ToTokenFnMut,
+};
+
+use proc_macro2::TokenStream as TokenStream2;
+
+use quote::{ToTokens,quote};
+
+use syn::Token;
+
+
+/// Constructs an RSlice constant.
+pub fn rslice_tokenizer<'a,I,T>(iter:I)->impl ToTokens+'a 
+where
+    I:IntoIterator<Item=T>+'a,
+    T:ToTokens,
+{
+    let mut iter=iter.into_iter();
+    ToTokenFnMut::new(move|ts|{
+        let comma=<Token![,]>::default();
+        let mut list=TokenStream2::new();
+        let mut len=0usize;
+        for elem in &mut iter {
+            elem.to_tokens(&mut list);
+            comma.to_tokens(&mut list);
+            len+=1;
+        }
+        let list=list.into_iter();
+
+        quote!(
+            unsafe{
+                abi_stable::std_types::RSlice::from_raw_parts_with_lifetime( 
+                    &[ #(#list)* ],
+                    #len
+                )
+            }
+        ).to_tokens(ts);
+    })
+}
+
+/// Constructs an RStr constant.
+pub fn rstr_tokenizer<S>(string:S)->impl ToTokens
+where
+    S:AsRef<str>
+{
+    ToTokenFnMut::new(move|ts|{
+        let string=string.as_ref();
+        let len=string.len();
+        quote!(
+            unsafe{
+                abi_stable::std_types::RStr::from_raw_parts( 
+                    #string.as_ptr(),
+                    #len
+                )
+            }
+        ).to_tokens(ts);
+    })
+}
