3fd9262633a7802c8aa1ded8ddb250c9560dcb88
rodrimati1992 <rodrimatt1985@gmail.com>
2019-09-11 17:11:13 -0300

    Added ConstGeneric,to represent and compare const parameters in type layouts.

--- abi_stable/src/abi_stability.rs
+++ abi_stable/src/abi_stability.rs
@@ -3,6 +3,7 @@ types and traits related to abi stability.
 */
 
 pub(crate) mod abi_checking;
+pub mod const_generics;
 pub mod extra_checks;
 pub mod get_static_equivalent;
 pub mod stable_abi_trait;
@@ -12,6 +13,7 @@ mod layout_tests;
 
 pub use self::{
     abi_checking::exported_check_layout_compatibility as check_layout_compatibility,
+    const_generics::{ConstGeneric,GetConstGenericVTable},
     get_static_equivalent::{GetStaticEquivalent_,GetStaticEquivalent},
     stable_abi_trait::{StableAbi,SharedStableAbi,AbiConsts},
 };
