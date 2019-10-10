f596659e86720b39b83b7386696d8131b024fec8
rodrimati1992 <rodrimatt1985@gmail.com>
2019-10-10 10:58:30 -0300

    Fixed travis bug,added `distance_from` function.
    The `distance_from` function calculates the distance in `T`s from one pointer to another.

--- abi_stable/src/utils.rs
+++ abi_stable/src/utils.rs
@@ -5,7 +5,7 @@ Utility functions.
 use std::{
     cmp::Ord,
     fmt::{self,Debug,Display},
-    mem::ManuallyDrop,
+    mem::{self,ManuallyDrop},
     ptr,
 };
 
@@ -223,6 +223,32 @@ where
     F:FnOnce()->R
 {}
 
+
+/// This function allows calculating the distance (in `T`s) from `from` to `to`.
+/// 
+/// This returns `None` if `from` has a higher address than `to`,
+/// or if `T` is a zero sized type.
+/// 
+/// # Example
+/// 
+/// ```
+/// use abi_stable::utils;
+/// 
+/// let arr=["hello","world","foo","bar","baz"];
+/// 
+/// assert_eq!(utils::distance_from(&arr[0],&arr[0]),Some(0));
+/// assert_eq!(utils::distance_from(&arr[0],&arr[4]),Some(4));
+///
+/// assert_eq!(utils::distance_from(&arr[4],&arr[0]),None);
+/// 
+/// ```
+pub fn distance_from<T>(from:*const T,to:*const T)->Option<usize>{
+    (to as usize)
+        .checked_sub(from as usize)?
+        .checked_div(mem::size_of::<T>())
+}
+
+
 //////////////////////////////////////////////////////////////////////
 
 #[cfg(not(feature="rust_1_38"))]
@@ -235,4 +261,33 @@ pub extern "C" fn get_type_name<T>()->RStr<'static>{
 #[doc(hidden)]
 pub extern "C" fn get_type_name<T>()->RStr<'static>{
     RStr::from(std::any::type_name::<T>())
+}
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn distance_from_(){
+        let int_array=[0,1,2,3,4];
+        let unit_array=[(),(),(),(),()];
+
+        for (ix,x) in int_array.iter().enumerate() {
+            for (iy,y) in int_array.iter().enumerate() {
+                if ix <= iy {
+                    assert_eq!(distance_from(x,y), Some(iy-ix));
+                }else{
+                    assert_eq!(distance_from(x,y), None);
+                }
+            }
+        }
+
+        for x in &unit_array {
+            for y in &unit_array {
+                assert_eq!(distance_from(x,y), None);
+            }
+        }
+
+    }
 }
