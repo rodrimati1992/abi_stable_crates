21b3ff69a1ccb431f7e9177e3bbfca7a24f4ef79
rodrimati1992 <rodrimatt1985@gmail.com>
2019-10-23 14:26:56 -0300

    Added documentation on library evolution.

--- abi_stable/src/docs.rs
+++ abi_stable/src/docs.rs
@@ -5,6 +5,7 @@ Documentation for macros,and guides.
 pub mod get_static_equivalent;
 pub mod prefix_types;
 pub mod unsafe_code_guidelines;
+pub mod library_evolution;
 pub mod stable_abi_derive;
 pub mod sabi_extern_fn;
 pub mod sabi_trait_attribute;
