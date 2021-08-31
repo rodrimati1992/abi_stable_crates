d7ec2d970b35c95f3bb74dd4b81b76304652d7ac
rodrimati1992 <rodrimatt1985@gmail.com>
2021-08-31 13:35:25 -0300

    Finished docs for RRef
    Added basic testing for all the methods

--- abi_stable/src/sabi_types/rref.rs
+++ abi_stable/src/sabi_types/rref.rs
@@ -11,6 +11,102 @@ use crate::{
 
 /// Equivalent to `&'a T`,
 /// which allows a few more operations without causing Undefined Behavior.
+/// 
+/// # Purpose
+/// 
+/// This type is used as the `&self` parameter in abi_stable trait objects
+/// because it can be soundly transmuted 
+/// to point to other smaller but compatible types, then back to the original type.
+/// 
+/// This crate is tested with [miri] to detect bugs in unsafe code,
+/// which implements the  [Stacked Borrows model].
+/// Because that model forbids `&T` to `&()`  to `&T` transmutes (when `T` isn't zero-sized),
+/// it required defining `RRef` to allow a reference-like type that can be transmuted.
+/// 
+/// # Example
+/// 
+/// This example demonstrates how a simple `&dyn Any`-like type can be implemented.
+/// 
+/// ```rust
+/// use abi_stable::{
+///     marker_type::ErasedObject,
+///     std_types::UTypeId,
+///     RRef,
+/// };
+/// 
+/// fn main(){ 
+///     let value = WithTypeId::new(5u32);
+///     let erased = value.erase();
+/// 
+///     assert_eq!(WithTypeId::downcast::<i32>(erased), None);
+///     assert_eq!(WithTypeId::downcast::<bool>(erased), None);
+///     assert_eq!(WithTypeId::downcast::<u32>(erased), Some(&value));
+/// }
+/// 
+/// // `#[repr(C))]` with a trailing `T` field is required for soundly transmuting from 
+/// // `RRef<'a, WithTypeId<T>>` to `RRef<'a, WithTypeId<ErasedObject>>`.
+/// #[repr(C)]
+/// #[derive(Debug, PartialEq)]
+/// struct WithTypeId<T>{
+///     type_id: UTypeId,
+///     value: T,
+/// }
+/// 
+/// impl<T> WithTypeId<T> {
+///     pub fn new(value: T) -> Self
+///     where
+///         T: 'static
+///     {
+///         Self{
+///             type_id: UTypeId::new::<T>(),
+///             value,
+///         }
+///     }
+/// 
+///     pub fn erase(&self) -> RRef<'_, WithTypeId<ErasedObject>> {
+///         unsafe{ RRef::new(self).transmute::<WithTypeId<ErasedObject>>() }
+///     }
+/// }
+/// 
+/// impl WithTypeId<ErasedObject> {
+///     pub fn downcast<T>(this: RRef<'_, Self>) -> Option<&WithTypeId<T>> 
+///     where
+///         T: 'static
+///     {
+///         if this.get().type_id == UTypeId::new::<T>() {
+///             // safety: we checked that type parameter was `T`
+///             unsafe{ Some(this.transmute_into_ref::<WithTypeId<T>>()) }
+///         } else {
+///             None
+///         }
+///     }
+/// }
+/// 
+/// 
+/// ```
+/// 
+/// <span id="type-prefix-exp"></span>
+/// # Type Prefix
+/// 
+/// A type parameter `U` is considered a prefix of `T` in all of these cases:
+/// 
+/// - `U` is a zero-sized type with an alignment equal or lower than `T`
+/// 
+/// - `U` is a `#[repr(transparent)]` wrapper over `T`
+/// 
+/// - `U` and `T` are both `#[repr(C)]` structs,
+/// in which `T` starts with the fields of `U` in the same order,
+/// and `U` has an alignment equal to or lower than `T`.
+/// 
+/// Please note that it can be unsound to transmute a non-local
+/// type if it has private fields,
+/// since it may assume it was constructed in a particular way.
+/// 
+/// [Stacked Borrows model]:
+/// https://github.com/rust-lang/unsafe-code-guidelines/blob/master/wip/stacked-borrows.md
+/// 
+/// [miri]: https://github.com/rust-lang/miri
+/// 
 #[repr(transparent)]
 #[derive(StableAbi)]
 #[sabi(
@@ -57,11 +153,37 @@ shared_impls! {
 
 
 impl<'a,T> RRef<'a,T>{
+    /// Constructs this RRef from a reference.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use abi_stable::sabi_types::RRef;
+    ///
+    /// struct GetPtr<'a,T>(&'a T);
+    ///
+    /// impl<'a,T:'a> GetPtr<'a,T>{
+    ///     const REF:&'a Option<T>=&None;
+    ///
+    ///     const STATIC:RRef<'a,Option<T>>=
+    ///         RRef::new(Self::REF);
+    /// }
+    ///
+    /// ```
+    #[inline(always)]
+    pub const fn new(ref_:&'a T)->Self{
+        Self{
+            ref_: ref_as_nonnull(ref_),
+            _marker:PhantomData,
+        }
+    }
+
     /// Constructs this RRef from a raw pointer.
     ///
     /// # Safety
     ///
-    /// You must ensure that the raw pointer is valid for the `'a` lifetime.
+    /// You must ensure that the raw pointer is valid for the `'a` lifetime,
+    /// and points to a fully initialized and aligned `T`.
     ///
     /// # Example
     ///
@@ -90,58 +212,17 @@ impl<'a,T> RRef<'a,T>{
         }
     }
 
-    /// Constructs this RRef from a reference.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use abi_stable::sabi_types::RRef;
-    ///
-    /// struct GetPtr<'a,T>(&'a T);
-    ///
-    /// impl<'a,T:'a> GetPtr<'a,T>{
-    ///     const REF:&'a Option<T>=&None;
-    ///
-    ///     const STATIC:RRef<'a,Option<T>>=
-    ///         RRef::new(Self::REF);
-    /// }
-    ///
-    /// ```
-    #[inline(always)]
-    pub const fn new(ref_:&'a T)->Self{
-        Self{
-            ref_: ref_as_nonnull(ref_),
-            _marker:PhantomData,
-        }
-    }
-
     /// Casts this to an equivalent reference.
     ///
     /// # Example
-    ///
-    /// ```
-    /// use abi_stable::sabi_types::RRef;
-    ///
-    /// struct GetPtr<'a,T>(&'a T);
-    ///
-    /// impl<'a,T:'a> GetPtr<'a,T>{
-    ///     const NONE_REF:&'a Option<T>=&None;
-    ///
-    ///     const REFERENCE:RRef<'a,Option<T>>=RRef::new(Self::NONE_REF);
-    ///
-    ///     // This returns a reference that lives as long as T does
-    ///     fn returns_ref()->&'a Option<String>{
-    ///         let reference=GetPtr::<String>::REFERENCE;
-    ///         reference.get()
-    ///     }
-    ///
-    ///     // This doesn't work,it borrows the reference `variable`.
-    ///     // fn returns_ref_2()->&'a Option<String>{
-    ///     //     let reference=GetPtr::<String>::REFERENCE;
-    ///     //     &*reference
-    ///     // }
-    /// }
-    ///
+    /// 
+    /// ```rust
+    /// use abi_stable::RRef;
+    /// 
+    /// let rref = RRef::new(&89);
+    /// 
+    /// assert_eq!(rref.get(), &89);
+    /// 
     /// ```
     #[inline(always)]
     pub fn get(self) -> &'a T{
@@ -149,6 +230,17 @@ impl<'a,T> RRef<'a,T>{
     }
 
     /// Copies the value that this points to.
+    /// 
+    /// # Example
+    /// 
+    /// ```rust
+    /// use abi_stable::RRef;
+    /// 
+    /// let rref = RRef::new(&55);
+    /// 
+    /// assert_eq!(rref.get_copy(), 55);
+    /// 
+    /// ```
     #[inline(always)]
     pub fn get_copy(self) -> T 
     where 
@@ -160,23 +252,15 @@ impl<'a,T> RRef<'a,T>{
     /// Casts this to an equivalent raw pointer.
     ///
     /// # Example
-    ///
-    /// ```
-    /// use abi_stable::sabi_types::RRef;
-    /// use std::convert::Infallible;
-    ///
-    /// struct GetPtr<'a,T>(&'a T);
-    ///
-    /// impl<'a,T:'a> GetPtr<'a,T>{
-    ///     const NONE_REF: &'a Option<T> = &None;
-    ///
-    ///     const STATIC: RRef<'a,Option<T>> =
-    ///         RRef::new(Self::NONE_REF);
+    /// 
+    /// ```rust
+    /// use abi_stable::RRef;
+    /// 
+    /// let rref = RRef::new(&89);
+    /// 
+    /// unsafe{
+    ///     assert_eq!(*rref.as_ptr(), 89);
     /// }
-    ///
-    /// let reference: *const Option<Infallible>=
-    ///     GetPtr::<Infallible>::STATIC.as_ptr();
-    ///
     /// ```
     #[inline(always)]
     pub const fn as_ptr(self) -> *const T{
@@ -185,32 +269,28 @@ impl<'a,T> RRef<'a,T>{
 
     /// Transmutes this `RRef<'a,T>` to a `RRef<'a,U>`.
     ///
-    /// This is equivalent to calling `transmute`,
-    /// except that it doesn't change the lifetime parameter of `RRef`.
-    ///
     /// # Safety
     ///
-    /// This has the same safety problems that transmuting `&'a T` to `&'a U` has.
-    ///
+    /// Either of these must be the case:
+    /// 
+    /// - [`U` is a prefix of `T`](#type-prefix-exp)
+    /// 
+    /// - `RRef<'a, U>` was the original type of this `RRef<'a, T>`.
+    /// 
     /// # Example
     ///
     /// ```
-    /// use abi_stable::sabi_types::RRef;
+    /// use abi_stable::RRef;
     ///
-    /// struct GetPtr<'a,T>(&'a T);
+    /// use std::num::Wrapping;
     ///
-    /// impl<'a,T:'a> GetPtr<'a,T>{
-    ///     const PTR:*const Option<T>=&None;
+    /// let rref = RRef::new(&13u32);
     ///
-    ///     const STATIC:RRef<'a,Option<T>>=unsafe{
-    ///         RRef::from_raw(Self::PTR)
-    ///     };
-    /// }
-    ///
-    /// let reference:RRef<'static,[();0xFFF_FFFF]>=unsafe{
-    ///     GetPtr::<'static,()>::STATIC
-    ///         .transmute::<[();0xFFF_FFFF]>()
-    /// };
+    /// // safety: Wrapping is a `#[repr(transparent)]` wrapper with one `pub` field.
+    /// let trans = unsafe{ rref.transmute::<Wrapping<u32>>() };
+    /// 
+    /// assert_eq!(trans, RRef::new(&Wrapping(13u32)));
+    /// 
     ///
     /// ```
     #[inline(always)]
@@ -230,6 +310,29 @@ impl<'a,T> RRef<'a,T>{
     }
 
     /// Transmutes this to a reference pointing to a different type.
+    ///
+    /// # Safety
+    ///
+    /// Either of these must be the case:
+    /// 
+    /// - [`U` is a prefix of `T`](#type-prefix-exp)
+    /// 
+    /// - `RRef<'a, U>` was the original type of this `RRef<'a, T>`.
+    /// 
+    /// # Example
+    ///
+    /// ```rust
+    /// use abi_stable::{
+    ///     RRef,
+    ///     std_types::Tuple2,
+    /// };
+    ///
+    /// unsafe{
+    ///     let reff = RRef::new(&Tuple2(3u32, 5u64));
+    ///     assert_eq!(reff.transmute_into_ref::<u32>(), &3u32);
+    /// }
+    ///
+    /// ```
     #[inline(always)]
     pub unsafe fn transmute_into_ref<U>(self) -> &'a U 
     where
@@ -290,6 +393,7 @@ mod tests {
         let reference = RRef::new(&8);
         
         assert_eq!(*reference.get(), 8);
+        assert_eq!(reference.get_copy(), 8);
         unsafe{
             assert_eq!(*reference.as_ptr(), 8);
         }
@@ -300,8 +404,19 @@ mod tests {
         let reference = RRef::new(&(!0u32));
 
         unsafe{
-            assert_eq!(*reference.transmute_into_raw::<i32>(), -1);
             assert_eq!(reference.transmute::<i32>().get_copy(), -1);
+            assert_eq!(*reference.transmute_into_raw::<i32>(), -1);
+            assert_eq!(reference.transmute_into_ref::<i32>(), &-1);
+        }
+    }
+
+    #[test]
+    fn as_ptr_impl(){
+        let reference = RRef::new(&89u32);
+
+        unsafe{
+            assert_eq!(*AsPtr::as_ptr(&reference), 89);
+            assert_eq!(AsPtr::as_rref(&reference), reference);
         }
     }
 }
