b6bde3fc4ead33c2727920734a647a23f611860b
rodrimati1992 <rodrimatt1985@gmail.com>
2022-05-14 16:58:32 -0300

    Fixed the FIXMEs
    Requires upstream fix described in
    https://github.com/rodrimati1992/abi_stable_crates/pull/83#issuecomment-1126802367
    to enable the "halfbrown" feature.

--- abi_stable/src/std_types/map.rs
+++ abi_stable/src/std_types/map.rs
@@ -831,23 +831,20 @@ pub struct Keys<'a, K: 'a, V: 'a> {
     inner: Iter<'a, K, V>,
 }
 
-// FIXME: add this back after making `Iter: Clone`?
-// FIXME(#26925) Remove in favor of `#[derive(Clone)]`
-// impl<K, V> Clone for Keys<'_, K, V> {
-//     #[inline]
-//     fn clone(&self) -> Self {
-//         Keys {
-//             inner: self.inner.clone(),
-//         }
-//     }
-// }
+impl<K, V> Clone for Keys<'_, K, V> {
+    #[inline]
+    fn clone(&self) -> Self {
+        Keys {
+            inner: self.inner.clone(),
+        }
+    }
+}
 
-// FIXME: add this back after making `Iter: Clone`?
-// impl<K: Debug, V> fmt::Debug for Keys<'_, K, V> {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         f.debug_list().entries(self.clone()).finish()
-//     }
-// }
+impl<K: Debug, V> fmt::Debug for Keys<'_, K, V> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(self.clone()).finish()
+    }
+}
 
 impl<'a, K, V> Iterator for Keys<'a, K, V> {
     type Item = &'a K;
@@ -884,23 +881,20 @@ pub struct Values<'a, K: 'a, V: 'a> {
     inner: Iter<'a, K, V>,
 }
 
-// FIXME: add this back after making `Iter: Clone`?
-// FIXME(#26925) Remove in favor of `#[derive(Clone)]`
-// impl<K, V> Clone for Values<'_, K, V> {
-//     #[inline]
-//     fn clone(&self) -> Self {
-//         Values {
-//             inner: self.inner.clone(),
-//         }
-//     }
-// }
+impl<K, V> Clone for Values<'_, K, V> {
+    #[inline]
+    fn clone(&self) -> Self {
+        Values {
+            inner: self.inner.clone(),
+        }
+    }
+}
 
-// FIXME: add this back after making `Iter: Clone`?
-// impl<K, V: Debug> fmt::Debug for Values<'_, K, V> {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         f.debug_list().entries(self.clone()).finish()
-//     }
-// }
+impl<K, V: Debug> fmt::Debug for Values<'_, K, V> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(self.clone()).finish()
+    }
+}
 
 impl<'a, K, V> Iterator for Values<'a, K, V> {
     type Item = &'a V;
